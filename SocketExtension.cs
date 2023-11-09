e9374793edc6abc8c7772eb91f56d8a352203a1c
assasinos <kubasem198@gmail.com>
2023-11-09 22:40:08 +0100

    Repair server crash when host closed connection before all responses were send

--- SocketExtension.cs
+++ SocketExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Sockets;
+﻿using System.Diagnostics;
+using System.Net.Sockets;
 using SimpleWebServer.Http;
 
 namespace SimpleWebServer;
@@ -9,27 +10,44 @@ public static class SocketExtension
     private static Task TimeoutTask = Task.Delay(TimeSpan.FromSeconds(1));
     public static async Task SendHttpResponse(this Socket socket, HttpResponse response, CancellationToken cancellationToken)
     {
-        
-        await socket.SendAsync(response.ResponseBytes);
-
-        //Read bytes send back from the client
-        byte[] buffer = new byte[1024];
-        int bytesRead;
-        do
+        try
         {
-            var dataReceived = await Task.WhenAny( socket.ReceiveAsync(buffer, SocketFlags.None),TimeoutTask );
-            
-            if (dataReceived == TimeoutTask)
+
+            //Check if the socket is still connected
+            if (!socket.Poll(1, SelectMode.SelectWrite))
             {
-                        
-                socket.Shutdown(SocketShutdown.Both);
-                socket.Close();
-                break;
+                return;
             }
-            bytesRead = ((Task<int>)dataReceived).Result;
-        } while (bytesRead > 0);
-        
-        
+
+            await socket.SendAsync(response.ResponseBytes);
+
+            //Read bytes send back from the client
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            do
+            {
+                var dataReceived = await Task.WhenAny(socket.ReceiveAsync(buffer, SocketFlags.None), TimeoutTask);
+
+                if (dataReceived == TimeoutTask)
+                {
+
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                    break;
+                }
+
+                bytesRead = ((Task<int>)dataReceived).Result;
+            } while (bytesRead > 0);
+
+
+        }
+        catch (Exception e)
+        {
+            //TODO: ADD logger
+            //Maybe change this in the future
+            Debug.WriteLine($"There was an error processing request\nError:\n{e}");
+            return;
+        }
         
 
     }
