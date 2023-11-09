8f67833dc628306bec0e3b635e04cbe9f8f008b0
assasinos <kubasem198@gmail.com>
2023-11-09 10:12:17 +0100

    Add Automatic Mime types to files

---
+++ Http/Mime/Mime.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Net.Mime;
+
+namespace SimpleApi.Http.Mime;
+
+public static class Mime
+{
+    public static string GetContentType(string fileExtension)
+    {
+        return fileExtension switch
+        {
+            
+            ".css" => "text/css",
+            ".js" => "text/javascript",
+            ".json" => MediaTypeNames.Application.Json,
+
+            #region Images
+            ".png" => "image/png",
+            ".jpg" => "image/jpeg",
+            ".jpeg" => "image/jpeg",
+            ".gif" => "image/gif",
+            ".webp" => "image/webp",
+            ".svg" => "image/svg+xml",
+            ".tiff" => "image/tiff",
+            ".tif" => "image/tiff",
+            ".ico" => "image/x-icon",
+            #endregion
+            
+            #region Videos
+            ".webm" => "video/webm",
+            #endregion
+            
+            ".html" => MediaTypeNames.Text.Html,
+            _ => MediaTypeNames.Text.Plain
+        };
+    }
+}
--- Server.cs
+++ Server.cs
@@ -1,36 +1,37 @@
 ﻿using System.Diagnostics;
 using System.Net;
+using System.Net.Mime;
 using System.Net.Sockets;
 using System.Text;
 using SimpleApi.Configuration;
 using SimpleApi.Http;
 using SimpleApi.Http.Headers.Response;
+using SimpleApi.Http.Mime;
 using HttpMethod = SimpleApi.Http.HttpMethod;
 
 namespace SimpleApi;
 
 public class Server
 {
-
     public static ServerConfiguration Configuration;
-    
+
     private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
-    
-    private readonly Dictionary<string,(Func<HttpRequest, HttpResponse> handler, HttpMethod method)> _routes = new();
-    
+
+    private readonly Dictionary<string, (Func<HttpRequest, HttpResponse> handler, HttpMethod method)> _routes = new();
+
     public Server(IPAddress ipAddress, int port, Cors? cors = null)
     {
         Configuration = new ServerConfiguration(ipAddress, port);
     }
 
-    public void RegisterRoute(string route, HttpMethod method , Func<HttpRequest, HttpResponse> handler)
+    public void RegisterRoute(string route, HttpMethod method, Func<HttpRequest, HttpResponse> handler)
     {
         if (!_routes.TryAdd(route.ToLower(), (handler, method)))
         {
             throw new Exception($"There was an error registering the route {route}");
         }
     }
-    
+
     /// <summary>
     /// Registers new route
     /// </summary>
@@ -38,42 +39,40 @@ public class Server
     /// <param name="method">HTTP method</param>
     /// <param name="path">Path to file</param>
     /// <exception cref="Exception">Throws an exception if route couldn't be registered</exception>
-    public void RegisterRoute(string route, HttpMethod method , string path)
+    public void RegisterRoute(string route, HttpMethod method, string path)
     {
-        if (!_routes.TryAdd(route.ToLower(), ((_ => HttpResponse.GetResponse(200,File.ReadAllBytes(path))), method)))
+        if (!_routes.TryAdd(route.ToLower(), ((_ => HttpResponse.GetResponse(200, File.ReadAllBytes(path),Mime.GetContentType(Path.GetExtension(path)))), method)))
         {
             throw new Exception($"There was an error registering the route {route}");
         }
     }
 
-    public void RegisterDirectory(string baseRoute = "/" , HttpMethod method = HttpMethod.GET, string path = "pages")
+    public void RegisterDirectory(string baseRoute = "/", HttpMethod method = HttpMethod.GET, string path = "pages")
     {
         var files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
         foreach (var file in files)
         {
-            var baseFilePath = file.Replace(path, "").Replace("\\","/").Replace(".html", "");
+            var baseFilePath = file.Replace(path, "").Replace("\\", "/").Replace(".html", "");
 
 
             string route;
-            
+
             //Check for index.html
             if (Path.GetFileName(file) == "index.html")
             {
-                route = Path.Combine(baseRoute, baseFilePath.Replace("/index",""));
-                
+                route = Path.Combine(baseRoute, baseFilePath.Replace("/index", ""));
                 RegisterRoute(route, method, file);
                 continue;
             }
 
             route = Path.Combine(baseRoute, baseFilePath);
-            
-            
+
+
             RegisterRoute(route, method, file);
         }
     }
-    
-    
-    
+
+
     public void Start()
     {
         Task.Run(async () =>
@@ -81,7 +80,6 @@ public class Server
             var tcpListener = new TcpListener(Configuration.IpAddress, Configuration.Port);
 
             tcpListener.Start();
-
 
 
             while (!_cancellationTokenSource.IsCancellationRequested)
@@ -95,21 +93,22 @@ public class Server
                 socket.Receive(bytes);
                 var data = Encoding.UTF8.GetString(bytes);
                 Debug.WriteLine($"Received:\n {data}\n");
-                
-                
-                if (!HttpRequest.TryParse(data,null, out var request))
+
+
+                if (!HttpRequest.TryParse(data, null, out var request))
                 {
-                    await socket.SendHttpResponse(HttpResponse.GetResponse(400, "Malformed Response"), _cancellationTokenSource.Token);
+                    await socket.SendHttpResponse(HttpResponse.GetResponse(400, "Malformed Response"),
+                        _cancellationTokenSource.Token);
                     continue;
                 }
-                
+
                 //404
                 if (!_routes.TryGetValue(request.Headers.Path.ToLower(), out var handler))
                 {
                     await socket.SendHttpResponse(HttpResponse.GetResponse(404, ""), _cancellationTokenSource.Token);
                     continue;
                 }
-                
+
                 //405
                 if (handler.method != request.Headers.Method)
                 {
@@ -117,7 +116,7 @@ public class Server
                     continue;
                 }
 
-                
+
                 //TODO: ADD logger
                 try
                 {
@@ -125,15 +124,17 @@ public class Server
                 }
                 catch (Exception e)
                 {
-                    #if DEBUG
-                    await socket.SendHttpResponse(HttpResponse.GetResponse(500, $"There was an error processing your request\nError:\n{e}"), _cancellationTokenSource.Token);
-                    #else
+#if DEBUG
+                    await socket.SendHttpResponse(
+                        HttpResponse.GetResponse(500, $"There was an error processing your request\nError:\n{e}"),
+                        _cancellationTokenSource.Token);
+#else
                     await socket.SendHttpResponse(HttpResponse.GetResponse(500, "There was an error processing your request"), _cancellationTokenSource.Token);
-                    #endif
-                    
+#endif
+
                     continue;
                 }
-                
+
                 await Task.Delay(1000, _cancellationTokenSource.Token);
             }
 
